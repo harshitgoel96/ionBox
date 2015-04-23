b9736c77e36c6bd49970c46ed30a65fea6262646
Harshit Goel <HarshitGoel96@yahoo.com>
2015-04-23 18:17:47 +0530

    updated you, prepaired stub for android plugin

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic','starter.controllers', 'ngCordova'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform,$cordovaSQLite ) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,6 +17,11 @@ angular.module('starter', ['ionic','starter.controllers', 'ngCordova'])
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
+	/*var db=$cordovaSQLite.openDB({name: "ionBox.db", androidDatabaseImplementation: 2});
+	db.transaction(function(tx) {
+	
+	});
+	db.closeDB();*/
   });
 })
 
